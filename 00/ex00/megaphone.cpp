69c2fabb719a4cdc60e472031b4f61ba9e7535cf
LouisMac <louisguillaume000@gmail.com>
2022-08-15 11:45:05 +0300

    00/ex00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,15 +6,36 @@
 /*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 15:09:01 by lguillau          #+#    #+#             */
-/*   Updated: 2022/06/14 14:58:05 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/08/14 21:03:56 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main()
+using namespace std;
+
+void	print_strings(string str)
 {
-	std::cout << "Hello World!" << std::endl;
-	std::cout << "Hello World!" << std::endl;
+	int	i;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+	cout << str;
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = 0;
+	if (ac == 1)
+		return (cout << "* LOUD AND UNBEARBLE FEEDBACK NOISE *" << endl, 0);
+	while (av[++i])
+		print_strings(av[i]);
+	cout << endl;
 	return (0);
 }
