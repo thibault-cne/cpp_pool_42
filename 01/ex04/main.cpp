f10421d2fc9019860d6289620c6e6e98f1af8065
l-gllme <louisguillaume000@gmail.com>
2022-09-26 13:42:27 +0200

    01 ex04

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 11:45:45 by lguillau          #+#    #+#             */
-/*   Updated: 2022/09/26 12:07:27 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/09/26 13:39:23 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,31 @@ int	main(int ac, char **av)
 	std::string	s1(av[2]);
 	std::string	s2(av[3]);
 
-	std::ifstream	ifs("o");
-	std::ofstream	cfs("replace");
+	std::ifstream	ifs(filename.c_str());
+	if (!ifs)
+		return (std::cout << "Invalid file" << std::endl, 0);
 
+	filename += ".replace";
+	std::ofstream	cfs(filename.c_str());
+	if (!cfs)
+	{
+		ifs.close();
+		return (std::cout << "Impossible to create file" << std::endl, 0);
+	}
+
+	std::string	line;
+	std::string	tmp;
+
+	while (getline(ifs, line))
+	{
+		if (line == s1)
+			cfs << s2 << std::endl;
+		else
+			cfs << line << std::endl;
+	}
 
 	ifs.close();
 	cfs.close();
-	
+
 	return (0);
 }
