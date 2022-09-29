edb9ab1970e6a848afa506b957ac76c4ca1ed77b
l-gllme <louisguillaume000@gmail.com>
2022-09-29 15:37:34 +0200

    01 ex04 edit

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 11:45:45 by lguillau          #+#    #+#             */
-/*   Updated: 2022/09/26 13:39:23 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/09/29 15:36:52 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,15 @@ int	main(int ac, char **av)
 	std::string	s1(av[2]);
 	std::string	s2(av[3]);
 
+	std::string	line;
+	int		prev;
+	int		found;
+
+	/* Open files */
+
+	if (s1.empty())
+		return (std::cout << "S1 is empty" << std::endl, 0);
+
 	std::ifstream	ifs(filename.c_str());
 	if (!ifs)
 		return (std::cout << "Invalid file" << std::endl, 0);
@@ -34,15 +43,25 @@ int	main(int ac, char **av)
 		return (std::cout << "Impossible to create file" << std::endl, 0);
 	}
 
-	std::string	line;
-	std::string	tmp;
+	/* Read and cut */
 
 	while (getline(ifs, line))
 	{
-		if (line == s1)
-			cfs << s2 << std::endl;
-		else
+		prev = 0, found = 0;
+		if (line.find(s1, 0) == std::string::npos)
 			cfs << line << std::endl;
+		else
+		{
+			while (found != (int)line.length() && ((found = line.find(s1, found)) != (int)std::string::npos))
+			{
+				cfs << line.substr(prev, found - prev);
+				found += s1.length();
+				prev = found;
+				cfs << s2;
+			}
+			cfs << line.substr(prev);
+			cfs << std::endl;
+		}
 	}
 
 	ifs.close();
