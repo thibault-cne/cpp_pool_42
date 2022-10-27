153f223fe793dc328bad2db3c63362ba79466e13
LinHome <louisguillaume000@gmail.com>
2022-10-27 19:25:38 +0200

    03 ex02

---
+++ 03/ex02/ClapTrap.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 13:39:51 by lguillau          #+#    #+#             */
+/*   Updated: 2022/10/27 18:07:20 by lguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# define COUT std::cout <<
+# define ENDL << std::endl
+
+class ClapTrap
+{
+	public:
+
+		ClapTrap();
+		ClapTrap(std::string str);
+	 	ClapTrap(const ClapTrap &c);
+		ClapTrap	&operator=(const ClapTrap &c);
+		~ClapTrap();
+
+		void	attack(const std::string &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+
+		std::string getName(void) const;
+		unsigned int	getHitPoints(void) const;
+		unsigned int	getEnergyPoints(void) const;
+		unsigned int	getAttackDamage(void) const;
+
+		void		setName(std::string str);
+		void		setEnergyPoints(unsigned int amount);
+		void		setHitPoints(unsigned int amount);
+		void		setAttackDamage(unsigned int amount);
+
+	private:
+
+		std::string	_name;
+		unsigned int	_hitPoints;
+		unsigned int	_energyPoints;
+		unsigned int	_attackDamage;
+};
+
+#endif
