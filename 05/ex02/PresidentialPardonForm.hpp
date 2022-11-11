b2ae9a9c646c7b23c46a2a1ebe91c77006029a65
l-gllme <louisguillaume000@gmail.com>
2022-11-11 17:11:12 +0100

    05 02 p

--- 05/ex02/PresidentialPardonForm.hpp
+++ 05/ex02/PresidentialPardonForm.hpp
@@ -6,7 +6,7 @@
 /*   By: lguillau <lguillau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:59:28 by lguillau          #+#    #+#             */
-/*   Updated: 2022/11/10 18:07:02 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/11/11 16:44:23 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ class PresidentialPardonForm : public AForm
 	virtual void	execute(const Bureaucrat &executor);
 	
 	private:
+	std::string	target;
 };
 
 #endif
