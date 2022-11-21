b70a928ea536251e64f0236528d0a057853d7e88
LinHome <louisguillaume000@gmail.com>
2022-11-21 20:56:54 +0100

    06

---
+++ 06/ex00/conv.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conv.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguillau <lguillau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 18:01:27 by lguillau          #+#    #+#             */
+/*   Updated: 2022/11/21 20:56:14 by lguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.hpp"
+
+void	convInt(std::string const s)
+{
+	std::cout << "int: " << std::endl;
+}
+
+void	convChar(std::string const s)
+{
+	std::cout << "char: " << std::endl;
+}
+
+void	convFloat(std::string const s)
+{
+	std::cout << "float: " << std::endl;
+}
+
+void	convDouble(std::string const s)
+{
+	std::cout << "double: " << std::endl;
+}
---
+++ 06/ex00/header.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguillau <lguillau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 18:00:01 by lguillau          #+#    #+#             */
+/*   Updated: 2022/11/21 18:03:53 by lguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#include <iostream>
+#include <string>
+
+void	convInt(std::sting const s);
+void	convFloat(std::sting const s);
+void	convDouble(std::sting const s);
+void	convChar(std::sting const s);
+
+#endif
