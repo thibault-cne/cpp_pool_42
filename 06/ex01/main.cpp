951958d51912f72c10f3390c2272a927d112c556
l-gllme <louisguillaume000@gmail.com>
2022-12-02 13:10:23 +0100

    06 01

---
+++ 06/ex01/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 12:57:22 by lguillau          #+#    #+#             */
+/*   Updated: 2022/12/02 13:10:04 by lguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <iomanip>
+
+struct	Data
+{
+
+};
+
+uintptr_t	serialize(Data *ptr)
+{
+}
+
+Data	*deserialize(uintptr_t raw)
+{
+}
+
+int	main(void)
+{
+	return (0);
+}
