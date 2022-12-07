2c4c70ee8bcb6ec34e679a4ad19a8b40cdaad577
l-gllme <louisguillaume000@gmail.com>
2022-12-07 13:24:45 +0100

    06 02

--- 06/ex02/Base.cpp
+++ 06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: lguillau <lguillau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 19:58:23 by lguillau          #+#    #+#             */
-/*   Updated: 2022/12/06 19:41:54 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/12/07 13:13:29 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ Base::~Base()
 	return ;
 }
 
-Base	*Base::generate(void)
+Base	*generate(void)
 {
 	srand(time(NULL));
 	int	rVar = rand() % 3 + 1;
@@ -33,7 +33,7 @@ Base	*Base::generate(void)
 		b = new B();
 		std::cout << "B created" << std::endl;
 	}
-	else if (rVar == 3)
+	else
 	{
 		b = new C();
 		std::cout << "C created" << std::endl;
@@ -41,12 +41,38 @@ Base	*Base::generate(void)
 	return (b);
 };
 
-void	Base::identify(Base *p)
+void	identify(Base *p)
 {
-	(void)p;
+	Base *a;
+	Base *b;
+	Base *c;
+	a = dynamic_cast<A*>(p);
+	if (a != NULL)
+	{
+		std::cout << "A" << std::endl;
+		return ;
+	}
+	b = dynamic_cast<B*>(p);
+	if (b != NULL)
+	{
+		std::cout << "B" << std::endl;
+		return ;
+	}
+	c = dynamic_cast<C*>(p);
+	if (c != NULL)
+	{
+		std::cout << "C" << std::endl;
+		return ;
+	}
 }
 
-void	Base::identify(Base &p)
+void	identify(Base &p)
 {
-	(void)p;
+	try {
+		p = dynamic_cast<A*>(p);
+		std::cout << "A" << std::endl;
+	}
+	catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 }
--- 06/ex02/main.cpp
+++ 06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lguillau <lguillau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 19:16:00 by lguillau          #+#    #+#             */
-/*   Updated: 2022/12/06 19:43:33 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/12/07 13:11:24 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 int	main(void)
 {
-	Base	b;
-	Base	*a;
-
-	a = b.generate();
-
+	Base *a;
+	
+	std::cout << "==== Pointer ====" << std::endl;
+	a = generate();
+	identify(a);
 	delete a;
+
+	std::cout << "==== Reference ====" << std::endl;
+
+	Base *c = generate();
+	identify(*c);
+	delete c;
 
 	return (0);
 }
