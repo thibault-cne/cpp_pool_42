4329f121de793a65e92b8103b75da04ac8785597
LinHome <louisguillaume000@gmail.com>
2022-12-11 18:05:53 +0100

    07 00

---
+++ 07/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguillau <lguillau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 14:06:10 by lguillau          #+#    #+#             */
+/*   Updated: 2022/12/11 16:58:30 by lguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "template.hpp"
+
+int	main(void)
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return (0);
+}
---
+++ 07/ex00/template.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   template.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguillau <lguillau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/11 16:50:27 by lguillau          #+#    #+#             */
+/*   Updated: 2022/12/11 17:00:01 by lguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEMPLATE_HPP
+# define TEMPALTE_HPP
+
+#include <iostream>
+
+template<typename T>
+void	swap(T &a, T &b)
+{
+	T	tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template<typename T>
+T	min(T const &a, const T &b)
+{
+	return ((a <= b) ? a : b);
+}
+
+template<typename T>
+T	max(T const &a, const T &b)
+{
+	return ((a >= b) ? a : b);
+}
+
+#endif
