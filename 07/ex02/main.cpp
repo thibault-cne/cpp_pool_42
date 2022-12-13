0d551a8204fd7ceda4eed7555c412303e0426400
LinHome <louisguillaume000@gmail.com>
2022-12-13 15:37:44 +0100

    07 02 100%

--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 16:06:48 by lguillau          #+#    #+#             */
-/*   Updated: 2022/12/13 00:15:39 by lguillau         ###   ########.fr       */
+/*   Updated: 2022/12/13 15:37:07 by lguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,42 @@
 
 int	main(void)
 {
-	Array<int>	arr(10);
-
-	try {
-		int	i = -1;
-
-		while (++i < 10)
-			arr[i] = i;
-		i = -1;
-		while (++i < 10)
-			std::cout << arr[i];
-		std::cout << std::endl;
-	}
-	catch (const std::exception& e)
 	{
-	    std::cerr << e.what() << '\n';
+		Array<int>	arr(10);
+	
+		try {
+			int	i = -1;
+
+			while (++i < 10)
+				arr[i] = i;
+			i = -1;
+			while (++i < 10)
+				std::cout << arr[i];
+			std::cout << std::endl;
+		}
+		catch (const std::exception& e)
+		{
+		    std::cerr << e.what() << '\n';
+		}
+	}
+	{
+		Array<char>	arr(10);
+		
+		try {
+			int	i = -1;
+
+			while (++i < 10)
+				arr[i] = 'a';
+			i = -1;
+			while (++i < 10)
+				std::cout << arr[i];
+			std::cout << std::endl;
+		}
+		catch (const std::exception& e)
+		{
+		    std::cerr << e.what() << '\n';
+		}
+
 	}
 
 	return (0);
