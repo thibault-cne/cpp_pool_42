9ce51a7eaa1bdb24e5b3701a4fed9b05cd081d8b
LinHome <louisguillaume000@gmail.com>
2022-12-13 16:03:07 +0100

    08 00 10%

---
+++ 08/ex00/easyfind.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguillau <lguillau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/13 15:41:51 by lguillau          #+#    #+#             */
+/*   Updated: 2022/12/13 16:01:51 by lguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+#include <vector>
+
+template <typename T>
+void	easyfind(T a, int b)
+{
+
+}
+
+#endif
