6b003042784221659061ffd17456bf3973e90166
l-gllme <louisguillaume000@gmail.com>
2022-12-15 16:37:05 +0100

    08 02 debut

---
+++ 08/ex02/MutantStack.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lguillau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 16:30:55 by lguillau          #+#    #+#             */
+/*   Updated: 2022/12/15 16:36:14 by lguillau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+class MutantStack : public stack
+{
+	public:
+
+	private:
+};
+
+#endif
